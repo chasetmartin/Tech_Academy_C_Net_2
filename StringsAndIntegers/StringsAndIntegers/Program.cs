8e96019f5fe99662311b23b0f6b3fcdc7a56eee9
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-09 14:28:45 -0800

    Try Catch Assignment

---
+++ StringsAndIntegers/StringsAndIntegers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringsAndIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a list of integers:
+            List<int> listofint = new List<int> { 12, 23, 65, 97, 24, 68, 23, 53 };
+            Console.WriteLine("I've created a list of integers. \nPlease input an integer and I will divide each number in my list by your input:");
+            try
+            {
+                int dividebynum = Convert.ToInt32(Console.ReadLine());
+                foreach (int item in listofint)
+                {
+                    Console.WriteLine(item + " Divided by " + dividebynum + " equals ~ " + item / dividebynum);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You did not enter a valid integer input");
+            }
+            Console.WriteLine("End of the Program, outside of try/catch");
+            Console.ReadLine();
+
+        }
+    }
+}
