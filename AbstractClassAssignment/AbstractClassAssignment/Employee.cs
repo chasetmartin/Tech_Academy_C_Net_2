b87e50f21a97e4d9060f9cd17c14ce85ead0ee5c
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-10 13:11:35 -0800

    Abstract Classes Update

--- AbstractClassAssignment/AbstractClassAssignment/Employee.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -7,5 +7,9 @@ namespace AbstractClassAssignment
     //Create an employee class and inherit from the Person class
     public class Employee : Person
     {
+        public override void SayName()
+        {
+            Console.WriteLine("Full Name: " + firstName + " " + lastName);
+        }
     }
 }
--- AbstractClassAssignment/AbstractClassAssignment/Person.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -9,10 +9,7 @@ namespace AbstractClassAssignment
         //Creating properties of First and Last Names for the abtract Person class
         public string firstName { get; set; }
         public string lastName { get; set; }
-        //Create a sayName Method
-        public void SayName()
-        {
-            Console.WriteLine("Name: " + firstName + " " + lastName);
-        }
+        //Create an abstract sayName Method
+        public abstract void SayName();
     }
 }
