4d742be09fa57b84cce7ccf5a5b6d11b87e8c995
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-10 09:58:03 -0800

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    public class Math
+    {
+        //writing our void method that takes two int parameters
+        public void voidMethod(int a, int b)
+        {
+            int c = a * 12;
+            //Our void method will not pass back variables, instead it will write to the screen
+            Console.WriteLine("Result of a math operation (x12) involving the first parameter: " + c);
+            Console.WriteLine("And here is the second parameter just diplayed to the screen: " + b);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We will instantiate our class and call our void method:");
+            //Instantiating our class
+            Math math = new Math();
+            //Calling the void method in our Math class, passing in two numbers:
+            math.voidMethod(20, 45);
+
+            //Now we will call the void method and specify the paramters by name
+            math.voidMethod(a: 89, b: 12);
+
+            Console.ReadLine();
+
+        }
+    }
+}
