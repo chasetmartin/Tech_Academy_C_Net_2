154e8a427345b9a41c772ec755bfe69f07b9961e
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-10 08:36:20 -0800

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        //create add 10 method for integer
+        public int DoMath(int num1)
+        {
+            int result = num1 + 10;
+            return result;
+        }
+
+        //create subtract 10 method for decimal input
+        public int DoMath(decimal num1)
+        {
+            int numint = Convert.ToInt32(num1);
+            int result = numint - 10;
+            return result;
+        }
+
+        //create multiply by 5 method for string input
+        public int DoMath(string num1)
+        {
+            int numint = Convert.ToInt32(num1);
+            int result = numint * 5;
+            return result;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Math class as mathoverload, which includes our DoMath we will overload and call 3 seperate times:
+            Math mathoverload = new Math();
+
+            //Ask the user for an integer to do some math on
+            Console.WriteLine("Type an integer and we'll call our DoMath method for the first time");
+            //Store user input as userInt
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("We will add 10 to your integer:");
+            //Call the DoMath method of mathoverload to add ten and store the result
+            int addingten = mathoverload.DoMath(userInt);
+            Console.WriteLine(userInt + " + 10 = " + addingten);
+
+            //Ask the user for a decimal to do some math on
+            Console.WriteLine("Type a decimal number and we'll call our DoMath method for the second time overloading to the decimal operation");
+            //Store user input as userDec
+            decimal userDec = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("We will subtract 10 from your decimal number:");
+            //Call the DoMath method of matheroverload to subtract ten and store the result
+            int subtractingten = mathoverload.DoMath(userDec);
+            Console.WriteLine(userDec + " - 10 = " + subtractingten);
+
+            //Ask the user for a string to convert to an integer
+            Console.WriteLine("Last, type another integer (whole number) and we'll treat it as a string and call our DoMath method for the third time, overloading to the string operation");
+            //Store user input as userNum
+            string userString = Console.ReadLine();
+
+            Console.WriteLine("We will multiply your number (treated as a string) by 5:");
+            //Call the DoMath method of matheroverload to multiply by 5 and store the result
+            int multiplybyfive = mathoverload.DoMath(userString);
+            Console.WriteLine(userString + " x 5 = " + multiplybyfive);
+
+            Console.ReadLine();
+        }
+    }
+}
