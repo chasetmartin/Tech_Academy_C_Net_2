fd5634bf786741b644c94a471d1f9c58a30bfdca
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-09 16:42:27 -0800

    Calling Methods Update

--- CallingMethods/CallingMethods/Math.cs
+++ CallingMethods/CallingMethods/Math.cs
@@ -6,18 +6,21 @@ namespace CallingMethods
 {
     public class Math
     {
+        //create add 10 method
         public int Add(int num1)
         {
             int result = num1 + 10;
             return result;
         }
 
+        //create subtract 10 method
         public int Sub(int num1)
         {
             int result = num1 - 10;
             return result;
         }
 
+        //create multiply by 5 method
         public int Mult(int num1)
         {
             int result = num1 * 5;
