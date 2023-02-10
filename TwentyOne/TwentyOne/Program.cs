c6fa0db8d220dd2dacf29cb57251190a109ee586
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-10 11:18:29 -0800

    TwentyOne Update

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -12,7 +12,7 @@ namespace TwentyOne
         {
             Deck deck = new Deck();
             deck.Shuffle(3);
-            
+
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
@@ -20,15 +20,5 @@ namespace TwentyOne
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
-
-        
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
