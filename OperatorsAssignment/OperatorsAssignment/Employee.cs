55a706a03d4fbd13bb2cb7707b94a328af6db0b6
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-13 11:15:09 -0800

    Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        //Creating ID, Fname, Lname properties for Employee class
+        public int employeeID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //Overloading the == operator to compare ID of Employee object
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            if (employee1.employeeID == employee2.employeeID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Overloading the != operator to compare IDs
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            if (employee1.employeeID != employee2.employeeID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets see if our overloaded operators work");
+            //Instantiating two objects of the Employee class
+            Employee bob = new Employee() { employeeID = 1, firstName= "Bob", lastName= "Smith" };
+            Employee john = new Employee() { employeeID = 2, firstName = "John", lastName = "Smith" };
+
+            //Using my overloaded operators to compare bob and john
+            Console.WriteLine("Using the overloaded == operator (should be a false comparison):");
+            Console.WriteLine(bob == john);
+            Console.WriteLine("Using hte overloaded != operator (should be a true comparison):");
+            Console.WriteLine(bob != john);
+
+            //Trying the same ID numbers to see if the logic still works
+            Employee jim = new Employee() { employeeID = 2, firstName = "Jim", lastName = "Smith" };
+            Console.WriteLine("Using == on two employees with the same ID but different names (should be true):");
+            Console.WriteLine(jim == john);
+
+            Console.ReadLine();
+        }
+    }
+}
