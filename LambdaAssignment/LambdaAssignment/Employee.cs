2e31112439cb6e9c69d8cebca1a37ea28819cc71
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-13 15:38:14 -0800

    Lambda Assignment

---
+++ LambdaAssignment/LambdaAssignment/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaAssignment
+{
+    //Creating employee class
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+    }
+    //Creating an employee list Constructor to create a starting list quickly in main program
+    public class EmployeeList
+    {
+        public EmployeeList() 
+        {
+            Employees = new List<Employee>();
+
+            List<string> fNames = new List<string>() { "Joe", "Jim", "Joe", "Jerry", "Joe", "Beth", "Bob", "John", "Joe", "Steve"};
+            List<string> lNames = new List<string>() { "Smith", "Martin", "Hayes", "Bill", "Johnson", "Brown", "Miller", "Davis", "Williams", "Jones" };
+            List<int> iDent = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Employee employee = new Employee();
+                employee.Id = iDent[i];
+                employee.fName = fNames[i];
+                employee.lName = lNames[i];
+                Employees.Add(employee);
+            }
+        }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ LambdaAssignment/LambdaAssignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lambda Assignment: \nInstantiate 10 Employees");
+            //Instantiate 10 employees using EmployeeList constructor (4 have the first name Joe)
+            EmployeeList employeelist = new EmployeeList();
+            //Print list of employees to the screen to confirm 10 Employees created:
+            foreach (Employee employee in employeelist.Employees)
+            {
+                Console.WriteLine(employee.fName + " " + employee.lName + " ID= " + employee.Id);
+            }
+
+            //using a foreach loop to create a new list of all employees with the first name of "Joe"
+            Console.WriteLine("Here we will iterate through the employee list and pull out those with the first name of Joe");
+            //New empty list
+            List<Employee> joeList = new List<Employee>();
+            foreach (Employee employee in employeelist.Employees)
+            {
+                if (employee.fName == "Joe")
+                {
+                    joeList.Add(employee);
+                }
+            }
+            //foreach loop to print results to the screen
+            foreach (Employee employee in joeList)
+            {
+                Console.WriteLine(employee.fName + " " + employee.lName);
+            }
+
+            Console.WriteLine("Now here is the same exercise using a Lambda function:");
+            //Now we will use a lambda function to do the same thing
+            List<Employee> joeListLambda = employeelist.Employees.Where(x => x.fName == "Joe").ToList();
+            //foreach loop to print lambda results to the screen
+            foreach (Employee employee in joeListLambda)
+            {
+                Console.WriteLine(employee.fName + " " + employee.lName);
+            }
+
+            Console.WriteLine("Now we will use a Lambda function to create a list of employees with Id > 5:");
+            //Lambda function to make a new list of employees with an Id > 5
+            List<Employee> greaterthanfive = employeelist.Employees.Where(x => x.Id > 5).ToList();
+            //Foreach loop to print lambda results to the screen
+            foreach (Employee employee in greaterthanfive)
+            {
+                Console.WriteLine(employee.fName + " " + employee.lName + " ID= " + employee.Id);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
