80e6d99ad1c85a946df21aec7d1d71914a8163f3
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-13 14:29:50 -0800

    TwentyOne Update

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -13,17 +13,6 @@ namespace TwentyOne
            
             Deck deck = new Deck();
 
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-
-            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
-
-            List<int> numberList = new List<int>() { 1, 2, 3, 535, 342, 23 };
-
-            int sum = numberList.Where(x => x > 20).Sum();
-        
-            Console.WriteLine(sum);
-
-
             //deck.Shuffle(3);
 
             //foreach (Card card in deck.Cards)
